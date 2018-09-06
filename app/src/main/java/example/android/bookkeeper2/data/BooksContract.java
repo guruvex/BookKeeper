8a80676526505a8b11998a86b60bf290d23ab470
james heath <guruvex@gmail.com>
2018-09-06 10:33:40 -0600

    first working commit

---
+++ app/src/main/java/example/android/bookkeeper2/data/BooksContract.java
@@ -0,0 +1,23 @@
+package example.android.bookkeeper2.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by james on 9/5/2018.
+ */
+
+public final class BooksContract implements BaseColumns {
+
+    private BooksContract () {}
+
+    public static final class BookEntry implements BaseColumns {
+        public final static String TABLE_NAME = "books";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMNS_BOOK_TITLE = "title";
+        public final static String COLUMNS_BOOK_AUTHOR = "author";
+        public final static String COLUMNS_BOOK_IBSN = "ibsn";
+        public final static String COLUMNS_BOOK_AUTHOR_COUNTRY = "country";
+        public final static String COLUMNS_BOOK_PRICE = "price";
+    }
+}
---
+++ app/src/main/java/example/android/bookkeeper2/data/BooksDBHelper.java
@@ -0,0 +1,44 @@
+package example.android.bookkeeper2.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import example.android.bookkeeper2.data.BooksContract.BookEntry;
+/**
+ * Created by james on 9/5/2018.
+ */
+
+public class BooksDBHelper extends SQLiteOpenHelper {
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "bookstore.db";
+
+    /** Database version if database table change increment version */
+    private static final int DATABASE_VERSION = 1;
+
+
+    public BooksDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDB) {
+        // Create a String that will build the books table
+        String SQL_CREATE_BOOKS_TABLE =  "CREATE TABLE " + BookEntry.TABLE_NAME + " ("
+                + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookEntry.COLUMNS_BOOK_TITLE + " TEXT NOT NULL, "
+                + BookEntry.COLUMNS_BOOK_AUTHOR + " TEXT, "
+                + BookEntry.COLUMNS_BOOK_IBSN + " INTEGER NOT NULL, "
+                + BookEntry.COLUMNS_BOOK_AUTHOR_COUNTRY + " TEXT,"
+                + BookEntry.COLUMNS_BOOK_PRICE + " TEXT NOT NULL); ";
+
+        // make the table
+        sqLiteDB.execSQL(SQL_CREATE_BOOKS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    // no on upgrade code yet
+    }
+}
