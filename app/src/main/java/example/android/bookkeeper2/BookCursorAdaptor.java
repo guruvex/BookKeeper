1c424be34708ef5f70a93789b1f825feff611d63
james heath <guruvex@gmail.com>
2018-09-18 11:46:12 -0600

    cursor loader working

---
+++ app/src/main/java/example/android/bookkeeper2/BookCursorAdaptor.java
@@ -0,0 +1,78 @@
+package example.android.bookkeeper2;
+
+        import android.content.Context;
+        import android.database.Cursor;
+        import android.text.TextUtils;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.CursorAdapter;
+        import android.widget.TextView;
+
+        import example.android.bookkeeper2.data.BooksContract.BookEntry;
+
+/**
+ * {@link PetCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of pet data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
+
+/**
+ * Created by james on 9/18/2018.
+ */
+
+public class BookCursorAdaptor extends CursorAdapter {
+    /**
+     * Constructs a new { BookCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public BookCursorAdaptor(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current pet can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView titleTextView = (TextView) view.findViewById(R.id.title);
+        TextView authorTextView = (TextView) view.findViewById(R.id.author);
+
+        // Find the columns of pet attributes that we're interested in
+        int titleColumnIndex = cursor.getColumnIndex(BookEntry.COLUMNS_BOOK_TITLE);
+        int authorColumnIndex = cursor.getColumnIndex(BookEntry.COLUMNS_BOOK_AUTHOR);
+
+        // Read the pet attributes from the Cursor for the current pet
+        String petName = cursor.getString(titleColumnIndex);
+        String petBreed = cursor.getString(authorColumnIndex);
+
+        // Update the TextViews with the attributes for the current pet
+        titleTextView.setText(petName);
+        authorTextView.setText(petBreed);
+    }
+}
--- app/src/main/java/example/android/bookkeeper2/MainActivity.java
+++ app/src/main/java/example/android/bookkeeper2/MainActivity.java
@@ -1,21 +1,23 @@
 package example.android.bookkeeper2;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import example.android.bookkeeper2.data.BooksDBHelper;
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     // make the DB object
     private BooksDBHelper mDBHelper;
+    // make cursor to use for cursor loader
+    private static final int BookLoader = 0;
+    BookCursorAdaptor mCursorAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
+
+        ListView listView = findViewById(R.id.list);
+        View emptyView = findViewById(R.id.empty_view);
+        // if theres no data
+        listView.setEmptyView(emptyView);
+
+        mCursorAdaptor = new BookCursorAdaptor(this,null);
+        listView.setAdapter(mCursorAdaptor);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,19 +54,27 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 startActivity(intent);
             }
         });
-        mDBHelper = new BooksDBHelper(this);
+        //mDBHelper = new BooksDBHelper(this);
+
+
+        // start loader
+        getLoaderManager().initLoader(BookLoader,null,this);
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        displayDatabaseInfo();
+        //displayDatabaseInfo();
     }
 
     /**
      * Temporary helper method to display information in the onscreen TextView about the state of
      * the pets database.
      */
+
+    /**
     private void displayDatabaseInfo() {
         // Create and/or open a database to read from it
         //SQLiteDatabase booksDB = mDBHelper.getReadableDatabase();
@@ -121,6 +143,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu.
@@ -137,12 +160,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.save:
                 insertBookData();
-                displayDatabaseInfo();
+                //displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
                 deleteAllBooks();
-                displayDatabaseInfo();
+                //displayDatabaseInfo();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -173,16 +196,21 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+
+        String [] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMNS_BOOK_TITLE,
+                BookEntry.COLUMNS_BOOK_AUTHOR };
+        return new CursorLoader(this, BookEntry.CONTENT_URI,projection,null,null,null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        mCursorAdaptor.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        mCursorAdaptor.swapCursor(null);
     }
 }
